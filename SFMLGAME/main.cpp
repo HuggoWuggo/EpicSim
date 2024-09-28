3e2a2185d0dff59e92ad038e55bdac4dbc2a0693
HuggoWuggo <hugo.lewczak@gmail.com>
2024-09-28 19:39:13 +1000

    V0.01

--- SFMLGAME/main.cpp
+++ SFMLGAME/main.cpp
@@ -13,15 +13,20 @@ sf::Vector2f Interpolate(const sf::Vector2<float>& pointA, const sf::Vector2<flo
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "IT MOVES!");
     window.setFramerateLimit(60);
 
     sf::RectangleShape shape(sf::Vector2f(50, 50));
-    shape.setFillColor(sf::Color::Green);
+    sf::RectangleShape end(sf::Vector2f(20, 20));
+
+    shape.setFillColor(sf::Color(0, 255, 0, 100));
+    end.setFillColor(sf::Color::Yellow);
+
     sf::Vector2f pointA = sf::Vector2f(10, 10), pointB = sf::Vector2f(500, 500);
     float factor = 0.f, speed = .01f;
 
     shape.setPosition(pointA);
+    end.setPosition(sf::Vector2f(pointB.x + 15.5, pointB.y + 15.5));
 
     sf::Clock clock;
 
@@ -34,12 +39,13 @@ int main()
                 window.close();
         }
 
-        window.clear();
-        window.draw(shape);
-        window.display();
-
         factor += (clock.getElapsedTime().asSeconds() * speed);
         shape.setPosition(Interpolate(pointA, pointB, factor));
+
+        window.clear();
+        window.draw(shape);
+        window.draw(end);
+        window.display();
     }
 
     return 0;
