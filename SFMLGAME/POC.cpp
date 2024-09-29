21e92db94585e25b1887250066ae23d140faaa74
HuggoWuggo <hugo.lewczak@gmail.com>
2024-09-29 18:36:35 +1000

    V0.5

--- SFMLGAME/POC.cpp
+++ SFMLGAME/POC.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 #include <SFML/Graphics.hpp>
 
@@ -12,52 +13,106 @@ sf::Vector2f Interpolate(const sf::Vector2<float>& pointA, const sf::Vector2<flo
     return pointA + (pointB - pointA) * factor;
 }
 
+class Dog {
+private:
+    sf::Texture dogTex;
+    sf::RectangleShape dogcol;
+    sf::RectangleShape dogOut;
+public:
+    sf::Sprite dogSpr;
+    float factor = 0.f, speed = .01f;
+    Dog() {
+        // Init texture
+        dogTex.loadFromFile("Resources/Sprites/dog.png");
+        dogTex.setRepeated(false);
+
+        // Init Sprite
+        dogSpr.setPosition(600, 20);
+        dogSpr.setScale(3, 3);
+        dogSpr.setTexture(dogTex);
+
+        // Init Outline
+        dogOut.setSize(sf::Vector2f(96, 96));
+        dogOut.setFillColor(sf::Color(0, 0, 0, 0));
+        dogOut.setOutlineColor(sf::Color::Black);
+        dogOut.setOutlineThickness(2);
+        
+        // Init Collision
+        dogcol.setSize(sf::Vector2f(85, 24));
+        dogcol.setFillColor(sf::Color(0, 0, 0, 0));
+        dogcol.setOutlineColor(sf::Color::Blue);
+        dogcol.setOutlineThickness(2);
+    }
+
+    void moveTowards(std::vector<sf::RectangleShape> placed) {
+        sf::RectangleShape rect = (sf::RectangleShape)placed.back();
+        sf::Vector2f pointB = rect.getPosition();
+        if (!dogcol.getGlobalBounds().intersects(rect.getGlobalBounds())) {
+            // Move towards factor
+            factor = speed;
+            dogSpr.setPosition(Interpolate(dogSpr.getPosition(), sf::Vector2f(pointB.x - 40, pointB.y - 40), factor));
+        }
+        else {
+            std::cout << "Hello World" << std::endl;
+        }
+    }
+
+    void update() {
+        dogOut.setPosition(dogSpr.getPosition().x, dogSpr.getPosition().y);
+        dogcol.setPosition(dogSpr.getPosition().x + 8, dogSpr.getPosition().y + 35);
+    }
+
+    void render(sf::RenderWindow& window) {
+        window.draw(dogSpr);
+        window.draw(dogOut);
+        window.draw(dogcol);
+    }
+};
+
 int main() {
     // Init window and events
-	sf::RenderWindow window(sf::VideoMode(800, 600), "It Works", sf::Style::Titlebar | sf::Style::Close);
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Chinese Simulator", sf::Style::Titlebar | sf::Style::Close);
     window.setFramerateLimit(60);
     
+    // Create a Vector of Rectangles
+    std::vector<sf::RectangleShape> placed;
+
     // Init grass texture
     sf::Texture grass;
     if (!grass.loadFromFile("Resources/Textures/grass.png"))
         std::cout << "Couldn't load texture: GRASS" << std::endl;
     grass.setRepeated(true);
 
+    // Init dog
+    Dog dog;
     // Init Background sprite
     sf::Sprite BKG;
     BKG.setTexture(grass);
     BKG.setScale(1.5, 1.5);
     BKG.setTextureRect(sf::IntRect(0, 0, 800, 600));
-
-    // Define moveTowards variables
-    sf::Vector2f pointA = sf::Vector2f(10, 10), pointB = sf::Vector2f(500, 500);
-    float factor = 0.f, speed = .005f;
-
-    // Define moveTowards shape 1
-    sf::RectangleShape shape(sf::Vector2f(50, 50));
-    shape.setFillColor(sf::Color(0, 0, 0, 100));
-    shape.setPosition(pointA);
-
-    // Define moveTowards shape 2
-    sf::RectangleShape end(sf::Vector2f(20, 20));
-    end.setFillColor(sf::Color::Yellow);
-    end.setPosition(sf::Vector2f(pointB.x + 15.5f, pointB.y + 15.5f));
     
-    // Define moveTowards clock
-    sf::Clock clock;
 
 	while (window.isOpen())
     {
+
+        // Poll events
         sf::Event event;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
+            else if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
+                sf::RectangleShape rect{ { 20, 20 } };
+                rect.setPosition(sf::Vector2f(sf::Mouse::getPosition(window)));
+                rect.setFillColor(sf::Color::Yellow);
+                placed.push_back(rect);
+            }
         }
 
-        // Move towards factor
-        factor += (clock.getElapsedTime().asSeconds() * speed /*Questionable function*/ / 2);
-        shape.setPosition(Interpolate(pointA, pointB, factor));
+        //Update
+        dog.update();
+        if (placed.size() != 0)
+            dog.moveTowards(placed);
 
         // Render
         window.clear();
@@ -66,10 +121,42 @@ int main() {
         window.draw(BKG);
 
         //Draw Sprites
-        window.draw(shape);
-        window.draw(end);
+        dog.render(window);
+        for (auto& e : placed) {
+            window.draw(e);
+        }
 
         window.display();
     }
 
 }
+
+//#include <vector>
+//#include <SFML/Graphics.hpp>
+//
+//int main()
+//{
+//    sf::RenderWindow window(sf::VideoMode(640, 480), "SFML works!");
+//    std::vector<sf::RectangleShape> rects;
+//
+//    while (window.isOpen())
+//    {
+//        sf::Event event;
+//        while (window.pollEvent(event))
+//        {
+//            if (event.type == sf::Event::Closed)
+//                window.close();
+//            else if (event.type == sf::Event::MouseButtonPressed &&
+//                event.mouseButton.button == sf::Mouse::Left) {
+                //sf::RectangleShape rect{ { 3, 3 } };
+                //rect.setPosition(sf::Vector2f(sf::Mouse::getPosition(window)));
+                //rect.setFillColor(sf::Color::Green);
+                //rects.push_back(rect);
+//            }
+//        }
+//        window.clear();
+//        for (const auto& r : rects)
+//            window.draw(r);
+//        window.display();
+//    }
+//}
