314fc212206da94641475657bebc7b434104c249
HuggoWuggo <hugo.lewczak@gmail.com>
2024-10-04 11:35:27 +1000

    E

--- SFMLGAME/GameLoop.h
+++ SFMLGAME/GameLoop.h
@@ -6,19 +6,7 @@
 #include <SFML/Graphics.hpp>
 
 #include "Dog.h"
-
-// Load Tilemap
-class TileMap : public sf::Drawable, public sf::Transformable
-{
-public:
-
-	bool load(const std::string& tileset, sf::Vector2u tileSize, const int* tiles, unsigned int width, unsigned int height);
-
-private:
-	sf::VertexArray m_vertices;
-	sf::Texture m_tileset;
-	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
-};
+#include "tilemap.h"
 
 class GameLoop
 {
---
+++ SFMLGAME/tilemap.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+// Load Tilemap
+class TileMap : public sf::Drawable, public sf::Transformable
+{
+public:
+
+	bool load(const std::string& tileset, sf::Vector2u tileSize, const int* tiles, unsigned int width, unsigned int height);
+
+private:
+	sf::VertexArray m_vertices;
+	sf::Texture m_tileset;
+	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+};
